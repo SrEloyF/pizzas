5a98d7ec084d2560e3da5078bdc6594c7c5bf568
SrEloyF <fabrizioeloys@gmail.com>
2024-11-19 23:37:23 +0000

    resolviendo conflictos

--- api/views.py
+++ api/views.py
@@ -81,25 +81,25 @@ class RepertorioListCreate(ListCreateView):
 #     def post(self, request):
 #         password = request.data.get("password")
 #         hashed_password = request.data.get("hashed_password")
-        
+
 #         if not password or not hashed_password:
 #             return Response({"error": "Password or hash not provided"}, status=status.HTTP_400_BAD_REQUEST)
 #         password_is_correct = check_password(password, hashed_password)
-        
+
 #         if password_is_correct:
 #             return Response({"message": "Password is correct"}, status=status.HTTP_200_OK)
 #         else:
 #             return Response({"message": "Password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
-        
+
 class RegistroView(APIView):
     def post(self, request):
         serializer = ClienteSerializer(data=request.data.copy())
         if serializer.is_valid():
             cliente = serializer.save()
-            
+
             tokens = get_tokens_for_user(cliente)
             response = Response({"detail": "Usuario registrado exitosamente"}, status=status.HTTP_201_CREATED)
-            
+
             response.set_cookie(
                 key='access_token',
                 value=tokens['access'],
@@ -125,18 +125,18 @@ class LoginView(APIView):
     def post(self, request):
         usuario = request.data.get('usuario')
         contrasena = request.data.get('contrasena')
-        
+
         try:
             cliente = Cliente.objects.get(usuario=usuario)
         except Cliente.DoesNotExist:
             return Response({'detail': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         if check_password(contrasena,cliente.contrasena):
             refresh = RefreshToken.for_user(cliente)
             access = refresh.access_token
-            
+
             response = Response({'detail':'inicio de sesión exitoso'},status=status.HTTP_200_OK)
-    
+
             response.set_cookie(
                 key='access_token',
                 value=str(access),
@@ -146,7 +146,7 @@ class LoginView(APIView):
                 max_age=60*4,
                 samesite='Lax',
             )
-            
+
             response.set_cookie(
                 key='refresh_token',
                 value=str(refresh),
@@ -155,7 +155,7 @@ class LoginView(APIView):
                 max_age=60*60*24*7,
                 samesite='Lax',
             )
-            
+
             return response
         else:
             return Response({'detail': 'Credenciales incorrectas'}, status=status.HTTP_401_UNAUTHORIZED)
@@ -163,21 +163,21 @@ class LoginView(APIView):
 class RefreshTokenView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
         refresh_token = request.COOKIES.get('refresh_token')
-        
+
         if refresh_token:
             request.data['refresh'] = refresh_token
-            
+
             response = super().post(request, *args, **kwargs)
-            
+
             if response.status_code == 200:
                 new_access_token = response.data.get('access', None)
                 # new_refresh_token = response.data.get('refresh', None)
-                
+
                 # print(new_refresh_token)
                 if new_access_token:
                     response.delete_cookie('access_token')
                     # response.delete_cookie('refresh_token')
-                    
+
                     response.set_cookie(
                         key='access_token',
                         value=new_access_token,
@@ -186,7 +186,7 @@ class RefreshTokenView(TokenRefreshView):
                         max_age=60*4,
                         samesite='Lax'
                     )
-                    
+
                     response.data = {'message': 'Access Token refreshed successfully'}
                 else:
                     response.data = {'detail': 'New tokens not found in response'}
